318e700e7bfe3d4191abcc34809d031a29be0b66
thinhbui95 <buiphuocthinhbk@gmail.com>
2023-03-24 11:49:24 +0700

    Update Coin98CurveV2.sol

--- contracts/CurveV2/Coin98CurveV2.sol
+++ contracts/CurveV2/Coin98CurveV2.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.0;
 import "./CurveV2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 
 
-contract Coin98CurveV2 is CurveV2, Ownable,ReentrancyGuard{
+contract Coin98CurveV2 is CurveV2, Ownable{
     using SafeMath for uint256;
 
     struct SwapParam {
@@ -21,7 +20,7 @@ contract Coin98CurveV2 is CurveV2, Ownable,ReentrancyGuard{
 
     constructor(address _weth) WethProvider(_weth) {}
 
-    function swap(uint amount0Out, uint amount1Out, address payable to,  bytes memory data) external nonReentrant payable {
+    function swap(uint amount0Out, uint amount1Out, address payable to,  bytes memory data) external  payable {
         SwapParam memory swapParam = abi.decode(data, (SwapParam));
         //require(swapParam.index == 9, "Invalid Route");
         uint256 preBalance = swapParam.toToken.balanceOf(address(this));
@@ -34,9 +33,9 @@ contract Coin98CurveV2 is CurveV2, Ownable,ReentrancyGuard{
         );
 
         uint256 balance = swapParam.toToken.balanceOf(address(this));
-        Utils.transferTokens(address(swapParam.toToken), to, balance - preBalance);
+        Utils.transferTokens(address(swapParam.toToken), to, (balance - preBalance));
 
-        }
+    }
 
     function withdrawStuckERC20(address _token) external payable onlyOwner{
         uint256 balanceERC20 = IERC20(_token).balanceOf(address(this));
@@ -49,7 +48,6 @@ contract Coin98CurveV2 is CurveV2, Ownable,ReentrancyGuard{
         }
     }
     }
-
 
 
 
